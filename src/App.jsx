1eb79da3d86fab00930dda3bf7a9a48c3f91d5f9
Vivy <Tomitaviorel@yahoo.com>
2023-03-07 11:30:46 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Chat from './component/chat';
+
 const App = () => {
-  return <div> New project</div>;
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/chat' element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
